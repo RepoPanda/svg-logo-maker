7bfe7af2db6c52c9776d443e1a46e57dfb35aaaa
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-06-17 14:21:42 -0700

    CLI updated and SvgDoc class has been defined

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,7 +1,11 @@
 const { Square, Triangle, Circle } = require("../lib/shapes.js");
 
 // will test the circle class
-describe("Circle", () => {});
+describe("Circle", () => {
+    it('should accept a color parameter',  () => {
+       const svgExpected = '<circle cx='
+    })
+});
 
 // will test the triangle class
 describe("Triangle", () => {});
