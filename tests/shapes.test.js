df4cbb1c4c1aed15c3c2133bbad15d513f9b3aab
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-06-17 12:04:02 -0700

    tests for shapes created but not deifined. CLI class has been created but not defined.sudo completed.

---
+++ tests/shapes.test.js
@@ -0,0 +1,10 @@
+const { Square, Triangle, Circle } = require("../lib/shapes.js");
+
+// will test the circle class
+describe("Circle", () => {});
+
+// will test the triangle class
+describe("Triangle", () => {});
+
+// will test the square class
+describe("Square", () => {});
